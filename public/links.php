569831d8b7fc7a4db09dbf8338b546f0f12852fa
Gabriel Flambo <gabriel.flambo@hotmail.com>
2018-04-04 20:45:50 -0300

    Estrutura Base

---
+++ public/links.php
@@ -0,0 +1,8 @@
+<link rel="icon" href="images/logo.png" type="image/gif" sizes="16x16">
+<!-- build:css css/estilos.min.css -->
+<link href="css/estilo.css" rel="stylesheet">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<!-- endbuild -->
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
---
+++ public/scripts.php
@@ -0,0 +1,5 @@
+<!-- build:js js/scripts.min.js -->
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/script.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<!-- endbuild -->
